74601e17546c2bed3da7cca41b8925bb1b7d6b2c
DBerfintastan <deniz.tastan@ogr.sakarya.edu.tr>
2023-11-09 00:00:46 +0300

    First Spring challenge finished

---
+++ Challenge_8/springrest/src/main/java/com/bilgeadam/springrest/repository/DersOgrenciRepository.java
@@ -0,0 +1,61 @@
+package com.bilgeadam.springrest.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.stereotype.Repository;
+
+import com.bilgeadam.springrest.model.*;
+
+@Repository
+public class DersOgrenciRepository
+{
+	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	@Autowired
+	public void setJdbcTemplate(@Qualifier(value = "bizimjdbctemplate") JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate)
+	{
+		this.jdbcTemplate = jdbcTemplate;
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+	}
+
+	public List<DersOgrenci> getAll()
+	{
+		return jdbcTemplate.query("select * from \"public\".\"DERS_OGR\" order by \"ID\" asc",
+				BeanPropertyRowMapper.newInstance(DersOgrenci.class));
+	}
+
+	public boolean deleteByID(long id)
+	{
+		String sql = "delete from \"public\".\"DERS_OGR\" where \"ID\" = :ID";
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("ID", id);
+		return namedParameterJdbcTemplate.update(sql, paramMap) == 1;
+	}
+
+	public DersOgrenci getByID(long id)
+	{
+		DersOgrenci dersogr = null;
+		String sql = "select * from \"public\".\"DERS_OGR\" where \"ID\" = :ABUZIDDIN";
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("ABUZIDDIN", id);
+		dersogr = namedParameterJdbcTemplate.queryForObject(sql, paramMap,
+				BeanPropertyRowMapper.newInstance(DersOgrenci.class));
+		return dersogr;
+	}
+
+	public boolean save(DersOgrenci dersogr)
+	{
+		String sql = "insert into \"public\".\"DERS_OGR\" (\"DEVAMSIZLIK\", \"OGR_ID\",\" NOTE\") values (:DEVAM, :OGRID, : NOTE)";
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("DEVAM", dersogr.getDEVAMSIZLIK());
+		paramMap.put("OGRID", dersogr.getOGR_ID());
+		paramMap.put("NOTE", dersogr.getNOTE());
+		return namedParameterJdbcTemplate.update(sql, paramMap) == 1;
+	}
+
+}
